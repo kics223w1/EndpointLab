79d27a7bd1ded01e339f921245118dbd62c44aa8
kics223w1 <hieppro138@gmail.com>
2024-11-14 13:20:28 +0700

    refactor: init dynamic endpoint

---
+++ api/dynamic.go
@@ -0,0 +1,49 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Dynamic struct {
+}
+
+func NewDynamic() *Dynamic {
+	return &Dynamic{}
+}
+
+
+func (d *Dynamic) HandleBase64(c *gin.Context) {
+
+}
+
+func (d *Dynamic) HandleBytes(c *gin.Context) {
+
+}
+
+func (d *Dynamic) HandleDeplay(c *gin.Context) {
+
+}
+
+func (d *Dynamic) HandleDrip(c *gin.Context) {
+
+}
+
+func (d *Dynamic) HandleLinks(c *gin.Context) {
+
+}
+
+func (d *Dynamic) HandleRange(c *gin.Context) {
+
+}
+
+func (d *Dynamic) HandleStreamBytes(c *gin.Context) {
+
+}
+
+func (d *Dynamic) HandleStream(c *gin.Context) {
+
+}
+
+func (d *Dynamic) HandleUuid(c *gin.Context) {
+
+}
